e380ebda419e2689ab792192925a341dcd022658
schen1880 <49198953+schen1880@users.noreply.github.com>
2020-05-11 16:06:22 -0500

    Display static data from PostRepository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,29 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository posts) {
+        postRepository = posts;
+    }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
 
+        List<Post> posts = postRepository.getAllPosts();
+
         modelMap.put("title", "Blog Post 1");
+        modelMap.put("posts", posts);
 
         return "home";
     }
